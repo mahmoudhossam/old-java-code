6b2d2ed57e9ea29a2dc0966dae15c9f1d79303b8
Mahmoud Hossam <mahmoudhossam@users.noreply.github.com>
2011-07-06 05:09:18 +0200

    Refactored MySQL class

--- src/test/BetterProgrammerTask.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class BetterProgrammerTask {
-
-    public static int countWords(String s) {
-        String[] words = s.split("\\s");
-        return words.length;
-    }
-}
