a0241332798af3a399fd85501ee65e9cd3f56c9c
mhh91 <mhh91@localhost>
2011-07-06 05:09:18 +0200

    Refactored MySQL class

--- src/test/API.java
+++ src/test/API.java
@@ -2,6 +2,7 @@ package test;
 
 import java.io.*;
 import java.util.*;
+
 /**
  * playing with the java API a bit
  * 
@@ -13,37 +14,35 @@ public class API {
         new API().dateTime();
     }
 
-    public void writeToFile(){
-            FileWriter fw = null;
-            try {
-                File file = new File("/home/mhh91/Desktop/test.txt");
-                fw = new FileWriter(file, true);
-                Scanner scanner = new Scanner(System.in);
-                System.out.print("Enter text to output to file: ");
-                fw.write(scanner.nextLine());
-                fw.write('\n');
-                fw.flush();
-                fw.close();
-                FileReader fr = new FileReader(file);
-                char[] buffer = new char[100];
-                fr.read(buffer);
-                for (char c : buffer) {
-                    if (c != 0) {
-                        System.out.print(c);
-                    }
-                }
-            } catch (IOException ex) {
-                
-            } finally {
-                try {
-                    fw.close();
-                } catch (IOException ex) {
-                    
+    public void writeToFile() {
+        FileWriter fw = null;
+        try {
+            File file = new File("/home/mhh91/Desktop/test.txt");
+            fw = new FileWriter(file, true);
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter text to output to file: ");
+            fw.write(scanner.nextLine());
+            fw.write('\n');
+            fw.flush();
+            fw.close();
+            FileReader fr = new FileReader(file);
+            char[] buffer = new char[100];
+            fr.read(buffer);
+            for (char c : buffer) {
+                if (c != 0) {
+                    System.out.print(c);
                 }
             }
+        } catch (IOException ex) {
+        } finally {
+            try {
+                fw.close();
+            } catch (IOException ex) {
+            }
+        }
     }
 
-    public void dateTime(){
+    public void dateTime() {
         Calendar cal = Calendar.getInstance();
         int yoom = cal.get(Calendar.DAY_OF_MONTH);
         int sa3a = cal.get(Calendar.HOUR);
--- src/test/Publish.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class Publish {
-
-    public static void main(String[] args) {
-        
-    }
-
-}
